5f2e2e0dd6aab5f936dc3adcb948bee6f2f7ed2b
hsingyingli <xingyingli855@gmail.com>
2022-10-20 20:12:44 +0800

    change fetch data order (order by title)

--- pages/api/note.ts
+++ pages/api/note.ts
@@ -12,7 +12,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { data, error } = await supabase.from("notes").select("*").eq("owner_id", process.env.OWNER_ID || "")
+  const { data, error } = await supabase.from("notes").select("*").eq("owner_id", process.env.OWNER_ID || "").order("title")
   if (error) {
     res.status(400).json({ data: null, error: error.message })
     return
